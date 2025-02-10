7d3275e39c0ad1102c02a661a45708764589b492
Emeric <emeric.pataut@gmail.com>
2025-02-10 18:40:06 +0100

    test(helpers/date): add tests for getMonth function

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -1,14 +1,22 @@
-/**
- * 
- */
 
+import { getMonth } from "./index";
+
+// Test de la fonction "getMonth" et non du composant, donc pas besoin de "render"
+
+
+// TESTS COMPLEMENTAIRES 
 describe("Date helper", () => {
     describe("When getMonth is called", () => {
         it("the function return janvier for 2022-01-01 as date", () => {
-            // to implement
+            const date = new Date("2022-01-01"); // Création de la date
+            const result = getMonth(date); // Appel de la fonction
+            expect(result).toBe("janvier"); // Vérification du résultat
         });
+
         it("the function return juillet for 2022-07-08 as date", () => {
-            // to implement
+            const date = new Date("2022-07-08"); // Création de la date
+            const result = getMonth(date); // Appel de la fonction
+            expect(result).toBe("juillet"); // Vérification du résultat
         });
     });
 })
