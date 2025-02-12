8929984dc470e0f0f0f31ee16b1ac8cf1b5fd61f
Emeric <emeric.pataut@gmail.com>
2025-02-12 16:51:23 +0100

    test(pages/home): add unit tests for displaying various elements

--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -28,17 +28,39 @@ describe("When Form is created", () => {
 });
 
 
+
+// TESTS COMPLEMENTAIRES 
 describe("When a page is created", () => {
-  it("a list of events is displayed", () => {
-    // to implement
+  it("a list of events is displayed", async () => {
+    render(<Home />);
+    // Recherche tous les éléments ayant l'attribut data-testid="card-testid"
+    const eventCards = await screen.findAllByTestId("card-testid"); 
+    expect(eventCards.length).toBeGreaterThan(0); 
+    // Vérifie qu'au moins une carte d'évènement est présente sur la page
   })
-  it("a list a people is displayed", () => {
-    // to implement
+
+  it("a list a people is displayed", async () => {
+    render(<Home />);
+    await screen.findByText("Samira");
+    // await screen.findByText("Jean-Baptiste");
+    await screen.findByText("Alice");
+    await screen.findByText("Luís");
+    await screen.findByText("Christine");
+    await screen.findByText("Isabelle");
+    // Vérifie le bon affichage des noms des membres de l'équipe
   })
-  it("a footer is displayed", () => {
-    // to implement
+
+  it("a footer is displayed", async () => {
+    render(<Home />);
+    await screen.findByText("Notre derniére prestation");
+    await screen.findByText("Contactez-nous");
+    // Vérifie si les titres "Notre derniére prestation" et "Contactez-nous" sont bien affichés
   })
-  it("an event card, with the last event, is displayed", () => {
-    // to implement
+
+  it("an event card, with the last event, is displayed", async () => {
+    render(<Home />);
+    const cardElement = await screen.getByTestId("card-testid");
+    expect(cardElement.classList.contains("EventCard--small")).toBe(true);
+    // Vérifie si l'element affiché est bien de type "small"
   })
 });
