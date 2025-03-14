74afcd38de8aabf96ec892d37e9f1fc79c19843d
Emeric <emeric.pataut@gmail.com>
2025-03-14 09:25:47 +0100

    chore(home): adjust tests and add comments

--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import Home from "./index";
 
 describe("When Form is created", () => {
@@ -33,16 +33,18 @@ describe("When Form is created", () => {
 describe("When a page is created", () => {
   it("a list of events is displayed", async () => {
     render(<Home />);
-    // Recherche tous les éléments ayant l'attribut data-testid="card-testid"
-    const eventCards = await screen.findAllByTestId("card-testid"); 
-    expect(eventCards.length).toBeGreaterThan(0); 
-    // Vérifie qu'au moins une carte d'évènement est présente sur la page
-  })
+    await waitFor(async () => {
+          // Recherche tous les éléments ayant l'attribut data-testid="card-testid"
+          const eventCards = await screen.findAllByTestId("card-testid"); 
+          expect(eventCards.length).toBeGreaterThan(0); 
+          // Vérifie qu'au moins une carte d'évènement est présente sur la page
+  });
+});
 
   it("a list a people is displayed", async () => {
     render(<Home />);
     await screen.findByText("Samira");
-    // await screen.findByText("Jean-Baptiste");
+    await screen.findByText("Jean-baptiste");
     await screen.findByText("Alice");
     await screen.findByText("Luís");
     await screen.findByText("Christine");
@@ -52,9 +54,13 @@ describe("When a page is created", () => {
 
   it("a footer is displayed", async () => {
     render(<Home />);
+    // Vérifie si les titres "Notre derniére prestation" et "Contactez-nous" sont bien affichés
     await screen.findByText("Notre derniére prestation");
     await screen.findByText("Contactez-nous");
-    // Vérifie si les titres "Notre derniére prestation" et "Contactez-nous" sont bien affichés
+    
+    // Vérifie qu'une balise <footer> est presente au sein de la page
+    const footerElement = screen.getByRole("contentinfo"); // "contentinfo" est le rôle ARIA de <footer>
+    expect(footerElement).toBeInTheDocument();
   })
 
   it("an event card, with the last event, is displayed", async () => {
@@ -64,3 +70,4 @@ describe("When a page is created", () => {
     // Vérifie si l'element affiché est bien de type "small"
   })
 });
+
